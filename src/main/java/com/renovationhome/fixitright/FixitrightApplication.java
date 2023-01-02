c7ab0af0854d459e083314df7fd0afd1c61d15bb
Mina K. Fahmy <minakaramm1@gmail.com>
2023-01-02 17:56:35 -0500

    # Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/renovationhome/fixitright/FixitrightApplication.java
@@ -0,0 +1,13 @@
+package com.renovationhome.fixitright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixitrightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixitrightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renovationhome/fixitright/FixitrightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renovationhome.fixitright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixitrightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
