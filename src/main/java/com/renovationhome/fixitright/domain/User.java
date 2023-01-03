e1a74ebceb8b3a90bebcf7f34b73543eadff04f5
Mina K. Fahmy <minakaramm1@gmail.com>
2023-01-03 17:27:01 -0500

    setting up dto

--- src/main/java/com/renovationhome/fixitright/domain/User.java
+++ src/main/java/com/renovationhome/fixitright/domain/User.java
@@ -1,5 +1,79 @@
 package com.renovationhome.fixitright.domain;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Table
+@Entity(name = "users")
 public class User {
+	private Long userId;
+	private String name;
+	private String phone;
+	private Address address;
+	private String email;
+	private String description;
+
+	@Column(length = 200)
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	@Column(length = 25)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(length = 15)
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@OneToOne(mappedBy = "user", cascade = { CascadeType.PERSIST, CascadeType.MERGE,
+			CascadeType.REMOVE }, orphanRemoval = true)
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Column(length = 45)
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
