ffbaccf2b53d4c036c20447831f66d69eb0d31d2
Mina K. Fahmy <minakaramm1@gmail.com>
2023-01-05 22:43:50 -0500

    sending form email

--- src/main/java/com/renovationhome/fixitright/web/UserController.java
+++ src/main/java/com/renovationhome/fixitright/web/UserController.java
@@ -1,12 +1,29 @@
 package com.renovationhome.fixitright.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.renovationhome.fixitright.domain.User;
+import com.renovationhome.fixitright.repository.UserRepository;
 
 @Controller
 public class UserController {
+	@Autowired
+	UserRepository userRepo;
 	@GetMapping("/home")
-	String getHome() {
+	String getHome(ModelMap model) {
+		model.put("user", new User());
 		return "home";
+	}
+
+	@PostMapping("/home#")
+	String postUser(User user) {
+		userRepo.save(user);
+		
+//		https://www.geeksforgeeks.org/spring-boot-sending-email-via-smtp/
+		return "redirect:/home";
 	}
 }
